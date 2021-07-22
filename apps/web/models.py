cd3775a6825b75eb64248d00c87e372345408590
khushmeeet <khushmeetsingh1996@gmail.com>
2021-07-23 02:52:34 +0530

    Implemented basic post and get methods

--- apps/web/models.py
+++ apps/web/models.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, HttpUrl, Field
 from mongoengine import *
 
 
-class WebModel(BaseModel):
+class WebAPIModel(BaseModel):
     id: int = Field(default_factory=uuid.uuid4, alias='_id')
     url: HttpUrl = Field(..., description='url of the webpage')
     content: str = Field(..., description='content of the webpage in HTML')
@@ -20,8 +20,10 @@ class WebModel(BaseModel):
         }
 
 
-class WebSchema(Document):
+class WebDBModel(Document):
     url = URLField()
     content = StringField()
     added_on = DateTimeField(required=True)
+    meta = {'collection': 'web'}
+
 
--- apps/web/routes.py
+++ apps/web/routes.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter
-from .models import WebSchema
+from fastapi import APIRouter, Body
+from .models import WebDBModel, WebAPIModel
+import datetime
+import json
 
 web = APIRouter()
 
 
-@web.get('/')
-def default():
-    print(WebSchema.objects)
-    return {'Hello': 'World'}
+@web.get('/', response_description='List all data in Web Collection')
+def list_data():
+    data = []
+    for obj in WebDBModel.objects:
+        data.append({'id': str(obj.id), 'url': obj.url, 'content': obj.content, 'added_on': obj.added_on})
+    return data
+
+@web.post('/new', response_description='Add data to Web Collection')
+def new_data(web_model: WebAPIModel = Body(...)):
+    data = WebDBModel(
+        url=web_model.url,
+        content=web_model.content,
+        added_on=datetime.datetime.now(datetime.timezone.utc)
+    ).save()
+    return data
